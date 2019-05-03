d7ebcb9bb453ab21c953b821eff32bfc9ea8e4a1
danieltorres31 <danieltorres31@outlook.com>
2019-05-02 21:33:26 -0300

    feat: Implementa validacao campos obrigatorios

--- v1/routes/AtendimentoRoute.js
+++ v1/routes/AtendimentoRoute.js
@@ -7,12 +7,28 @@ const {check} = require('express-validator/check');
 
 router.route('/')
     .get(controller.consultar)
-    .post(controller.inserir);
+    .post([
+        check('quadroGeral', mensagens.QUADRO_GERAL_VAZIO).not().isEmpty(),
+        check('doencas').custom((value) => controller.validaAtributoDoenca(value, 'medicamento')),
+        check('doencas').custom((value) => controller.validaAtributoDoenca(value, 'posologiaRelatada')),
+        check('doencas').custom((value) => controller.validaAtributoDoenca(value, 'efetividade')),
+        check('doencas').custom((value) => controller.validaAtributoDoenca(value, 'seguranca')),
+        check('doencas').custom((value) => controller.validaAtributoDoenca(value, 'dificuldadeUso')),
+        check('doencas').custom((value) => controller.validaAtributoDoenca(value, 'prm')),
+    ], controller.inserir);
 
 router.route('/:id')
     .get(controller.consultarPorId)
     .delete(controller.excluir)
-    .put(controller.alterar);
+    .put([
+        check('quadroGeral', mensagens.QUADRO_GERAL_VAZIO).not().isEmpty(),
+        check('doencas').custom((value) => controller.validaAtributoDoenca(value, 'medicamento')),
+        check('doencas').custom((value) => controller.validaAtributoDoenca(value, 'posologiaRelatada')),
+        check('doencas').custom((value) => controller.validaAtributoDoenca(value, 'efetividade')),
+        check('doencas').custom((value) => controller.validaAtributoDoenca(value, 'seguranca')),
+        check('doencas').custom((value) => controller.validaAtributoDoenca(value, 'dificuldadeUso')),
+        check('doencas').custom((value) => controller.validaAtributoDoenca(value, 'prm')),
+    ],controller.alterar);
 
 router.route('/paciente/:id')
     .get(controller.buscaUltimoAtendimentoPorIdPaciente);
