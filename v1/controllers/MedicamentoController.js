1c6ace2c3d79228000b74690cf099a49e8920dac
Henrique Batista <henrique.batista@epoca.dc>
2019-05-24 15:51:43 -0300

    Feat: Recria component medicamento

---
+++ v1/controllers/MedicamentoController.js
@@ -0,0 +1,66 @@
+const mongoose = require('mongoose');
+const {
+    validationResult
+} = require('express-validator/check');
+
+const Medicamento = require('../models/Medicamento');
+
+class MedicamentoController {
+    async inserir(req, res) {
+        try {
+            const erros = validationResult(req);
+
+            if (!erros.isEmpty())
+                return res.status(422).json({
+                    errors: erros.array()
+                });
+
+            mongoose.connect(process.env.DB_URL, {
+                useNewUrlParser: true
+            });
+
+            let newMedicamento = new Medicamento({
+                ...req.body
+            })
+
+            newMedicamento = await newMedicamento.save();
+            res.json(newMedicamento);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    }
+
+    async consultar(req, res) {
+        try {
+            mongoose.connect(process.env.DB_URL, {
+                useNewUrlParser: true
+            });
+
+            const query = Medicamento.find({}, {'descricao':1});
+            const medicamento = await query.exec();
+            res.json(medicamento);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    }
+
+    excluir(req, res) {
+        try {
+            mongoose.connect(process.env.DB_URL, {useNewUrlParser: true});
+            
+            Medicamento.deleteOne({_id: mongoose.Types.ObjectId(req.params.id)}, (err, result) => {
+                if(err)
+                    return res.status(500).json({errors: [{...err}]});
+                
+                if(result.deletedCount == 0)
+                   return res.status(404).json(result); 
+
+                return res.json(result);
+            });
+        } catch(err) {
+            res.status(500).json(err);
+        }
+    }
+}
+
+module.exports = new MedicamentoController();
