57c609a65917c757346169b69fce44947716c682
Henrique Batista <henrique.batista@epoca.dc>
2019-06-13 14:40:24 -0300

    Fix: Ajusta Model medicamento

--- v1/models/Medicamento.js
+++ v1/models/Medicamento.js
@@ -1,12 +1,10 @@
 const mongoose = require('mongoose')
 
 const medicamentoSchema = new mongoose.Schema({
-    descricao: {
-        type: String,
-        unique: true
-    },
-    formaFarmaceuticaDosagem: String
-    
+    descricao: String,
+    formaFarmaceuticaDosagem: {
+        type: String
+    }
 })
 
 module.exports = mongoose.model('Medicamento', medicamentoSchema)
