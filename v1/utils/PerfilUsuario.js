1feb94a8b079f5698bb815742fe0b7f8aaf9210b
danieltorres31 <danieltorres31@outlook.com>
2019-06-23 15:14:40 -0300

    feat: Cria arquivo de perfis de usuário

---
+++ v1/utils/PerfilUsuario.js
@@ -0,0 +1,6 @@
+module.exports = {
+    ADMINISTRADOR: 'Administrador',
+    GESTOR_INSTITUICAO: 'Gestor da Instituição',
+    PROFISSIONAL_SAUDE: 'Profissional de Saúde',
+    ACADEMICO: 'Acadêmico'
+}
